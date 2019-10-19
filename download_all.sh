64f182f5af7db0b38e1c76347e8a9446ae18bb70
Arya McCarthy <arya.mccarthy@gmail.com>
2019-10-19 18:03:37 -0400

    Add support for macOS grep

--- download_all.sh
+++ download_all.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Download the first 100 repos, in serial so GitHub doesn't get mad.
-# Max results per page is 100. See https://developer.github.com/v3/#pagination
-curl -s "https://api.github.com/orgs/unimorph/repos?per_page=100&page=1" | 
-  grep ssh_url | 
-  grep -oP 'git@github.com:unimorph/.{3}.git' |  # Pattern only matches language repos, ignoring utility repos.
-  xargs -I@ bash -c "git clone @; sleep 5"
+# Getting "Permission denied (publickey)."?
+# Make sure that you've registered your public key with GitHub!
 
-# Download the rest.
-curl -s "https://api.github.com/orgs/unimorph/repos?per_page=100&page=2" | 
+# Max results per page is 100. See https://developer.github.com/v3/#pagination
+# So we concatenate the 100-item lists.
+# If there are ever more than 200 repos, we should increment this number.
+for i in {1..2}
+do
+  curl -s "https://api.github.com/orgs/unimorph/repos?per_page=100&page=$i"
+done |
   grep ssh_url | 
-  grep -oP 'git@github.com:unimorph/.{3}.git' | 
+  grep -o 'git@github.com:unimorph/[a-z]\{3\}.git' |  # Pattern only matches language repos, ignoring utility repos.
   xargs -I@ bash -c "git clone @; sleep 5"
